644f7fe48019c4ab681f868701393b524ff72682
nejbolit <nejbolit@DESKTOP-HJK3A9M>
2020-10-11 21:52:04 +0700

    Закончил лаб2

--- src/main/java/lab_2/Zadanie_1.java
+++ src/main/java/lab_2/Zadanie_1.java
@@ -1,10 +1,14 @@
-
 package lab_2;
 
 /**
  *
- * @author nejbolit
+ * @author Zhivotyagin
  */
 public class Zadanie_1 {
-    
+    public static void main(String[] args) {
+        String text="а=1, c=a+1 m=2, n=3 решение уравнений", 
+                text2="b=6, d=b*2-1 p=4, g=5 нахождение корней";
+        System.out.println(text + "\n" +text2);
+        
+    }
 }
---
+++ src/main/java/lab_2/Zadanie_2.java
@@ -0,0 +1,12 @@
+package lab_2;
+
+/**
+ *
+ * @author Zhivotyagin
+ */
+public class Zadanie_2 {
+    public static void main(String[] args) {
+        System.out.println("\t" +"\t"+"\t"+ 1 + "\n"+"\t"+"\t"+1+"\t"+2+"\t"+3+"\n"+"\t"+1+"\t"+2+"\t"+3+"\t"+4+"\t"+5+"\n"+1+"\t"+2+"\t"+3+"\t"+4+"\t"+5+"\t"+6+"\t"+7);  
+    }
+    
+}
